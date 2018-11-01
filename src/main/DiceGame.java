c58f315aa12220c72068376660a22131bcbe291b
Yurtsiv Stepan <mail@yurtsiv.me>
2018-11-01 19:13:56 +0200

    added basic Player class

--- src/main/DiceGame.java
+++ src/main/DiceGame.java
@@ -1,8 +1,9 @@
 package main;
 
-import main.Dices;
-
 public class DiceGame {
+    private Dices dices = new Dices(5);
+    private Player[] players;
+
     public static void main(String[] args) {
 	// write your code here
     }
---
+++ src/main/Player.java
@@ -0,0 +1,9 @@
+package main;
+
+public class Player {
+    private String name;
+
+    public Player (String name) {
+        this.name = name;
+    }
+}
