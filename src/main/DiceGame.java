b795a65635869d491de74d2830b10cedf4cb0ecf
Yurtsiv Stepan <mail@yurtsiv.me>
2018-11-01 19:28:56 +0200

    simple game initialization

--- src/main/DiceGame.java
+++ src/main/DiceGame.java
@@ -1,10 +1,32 @@
 package main;
 
+import java.util.Scanner;
+
 public class DiceGame {
     private Dices dices = new Dices(5);
     private Player[] players;
+    private int currentTurn = 0, maxTurns;
+
+    public void initialize () {
+        int playersAmount;
+        String playerName;
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("Amount of players (2-4):");
+        playersAmount = userInput.nextInt();
+        this.players = new Player[playersAmount];
+        for (int i = 0; i < playersAmount; i++) {
+            System.out.println("Player " + i + "'s name:");
+            playerName = userInput.next();
+            this.players[i] = new Player(playerName);
+        }
+
+        System.out.println("Turns amount:");
+        this.maxTurns = userInput.nextInt();
+    }
 
     public static void main(String[] args) {
-	// write your code here
+	    DiceGame game = new DiceGame();
+	    game.initialize();
     }
 }
